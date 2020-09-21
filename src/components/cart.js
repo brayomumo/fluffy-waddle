b1ec5cda65560998bd078620644ce783a8c3c08b
brayomumo <brayomumo5@gmail.com>
2020-09-21 10:55:45 +0300

    create cart

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,18 +1,5 @@
 import React from 'react'
-import size from './size'
+import cart from './size'
 
-class Cart extends React.Component{
-    constructor(props){
-        super (props);
-        this.state = {
-            totalPrice: 0,
-            items: {}
-        }
-    };
-    render(){
-        return (<div>
 
-        </div>)
-    }
-}
-export default Cart
+// export default Cart
