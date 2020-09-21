fe8940e2f33c140dfee37bb00359078d336229c7
brayomumo <brayomumo5@gmail.com>
2020-09-21 10:56:02 +0300

    Bootstrapping

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './App.css';
-// Components import 
-// import HelloWorld from './components/hello'
-import Sizes from './components/size'
+// // Components import 
+// import {cartt } from './components/size'
+import Cart from './components/size'
 // import CommentBox from './components/comment/components';
 
 function App() {
   return (
     <div className="App">
-    <Sizes/>
+      
+    {/* <Sizes/> */}
+    <Cart/>
      {/* <CommentBox /> */}
     </div>
   );
 }
 
 export default App;
+
